91e6709dccebaf0837040d027a0477f8ce84acd4
dave-aa <dave@artificial.agency>
2025-09-09 13:24:58 -0600

    add jest config file

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'js'],
+  testMatch: ['**/*.test.ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.test.ts'
+  ],
+  clearMocks: true,
+};
