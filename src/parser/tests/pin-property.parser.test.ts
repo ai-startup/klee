a8514e7c159518ff8c20823613d28d451e4f804f
dave-aa <dave@artificial.agency>
2025-10-17 12:11:29 -0600

    add tests for PinPropertyParser regex cases

--- src/parser/tests/pin-property.parser.test.ts
+++ src/parser/tests/pin-property.parser.test.ts
@@ -3,6 +3,70 @@
 import { PinPropertyParser } from '../../parser/pin-property.parser';
 import { PinDirection } from '../../data/pin/pin-direction';
 
+describe('PinPropertyParser - Regex Value Parsing', () => {
+    let parser: PinPropertyParser;
+
+    beforeEach(() => {
+        parser = new PinPropertyParser();
+    });
+
+    test('Type 1: Can parse quoted values', () => {
+        const propertyData = 'PinId=ABC123,PinName="self"';
+        const pinProperty = parser.parse(propertyData, 'TestNode');
+
+        expect(pinProperty).toBeDefined();
+        expect(pinProperty.name).toBe('Self'); // Parser applies prettifyText which capitalizes
+    });
+
+    test('Type 2: Can parse values set in brackets', () => {
+        const propertyData = 'PinId=ABC123,PinName="TestPin",LinkedTo=(K2Node_CallFunction_0 ABC12300000000000000000000000000,K2Node_Event_1 789DEF123,)';
+        const pinProperty = parser.parse(propertyData, 'TestNode');
+
+        expect(pinProperty).toBeDefined();
+        expect(pinProperty.linkedTo).toBeDefined();
+        expect(Array.isArray(pinProperty.linkedTo)).toBe(true);
+        expect(pinProperty.linkedTo.length).toBe(2);
+    });
+
+    test('Type 3: Can parse multilevel loctext', () => {
+        const propertyData = 'PinName="TestPin",PinFriendlyName=LOCGEN_FORMAT_NAMED(NSLOCTEXT("KismetSchema", "SplitPinFriendlyNameFormat", "{PinDisplayName} {ProtoPinDisplayName}"), "PinDisplayName", NSLOCTEXT("", "E767B2BA4B1D5DFDD5E21E953300AB1E", "Settings"), "ProtoPinDisplayName", NSLOCTEXT("", "182F932842DA4BEA8624D89F6CD70FDA", "Attenuation Settings"))';
+        const pinProperty = parser.parse(propertyData, 'TestNode');
+
+        expect(pinProperty).toBeDefined();
+        // Format string {PinDisplayName} {ProtoPinDisplayName} gets keys replaced with prettified values
+        expect(pinProperty.friendlyName).toBe('Proto Pin Display Name Attenuation Settings');
+    });
+
+    test('Type 4: Can parse method values', () => {
+        const propertyData = 'PinName="self",PinFriendlyName=NSLOCTEXT("K2Node", "Target", "Target")';
+        const pinProperty = parser.parse(propertyData, 'TestNode');
+
+        expect(pinProperty).toBeDefined();
+        expect(pinProperty.friendlyName).toBe('Target');
+    });
+
+    test('Type 5: Can parse pure values', () => {
+        const propertyData = 'PinId=123,PinName="TestPin",PinType.bIsConst=False,PinType.bIsReference=True';
+        const pinProperty = parser.parse(propertyData, 'TestNode');
+
+        expect(pinProperty).toBeDefined();
+        expect(pinProperty.isConst).toBe(false);
+        expect(pinProperty.isReference).toBe(true);
+    });
+
+    test('Can parse complex property with multiple value types', () => {
+        const propertyData = 'PinId=ABC123,PinName="ComplexPin",PinFriendlyName=NSLOCTEXT("", "12345", "Friendly Name"),LinkedTo=(Node1 GUID1,),PinType.bIsConst=False,DefaultValue="test"';
+        const pinProperty = parser.parse(propertyData, 'TestNode');
+
+        expect(pinProperty).toBeDefined();
+        expect(pinProperty.name).toBe('Complex Pin');
+        expect(pinProperty.friendlyName).toBe('Friendly Name');
+        expect(pinProperty.linkedTo).toBeDefined();
+        expect(pinProperty.isConst).toBe(false);
+        expect(pinProperty.defaultValue).toBe('test');
+    });
+});
+
 describe('PinPropertyParser - serializeAsSinglePrecisionFloat', () => {
     let parser: PinPropertyParser;
 
